27a42f37625b1cb49b9bba037498c1860296afeb
JoseMario3 <f.jose@wustl.edu>
2022-06-13 17:48:39 -0500

    Week 2 Lab 1, End of Day

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import * as React from "react"
+import * as React from "react";
 // IMPORT ANY NEEDED COMPONENTS HERE
-import { createDataSet } from "./data/dataset"
-import "./App.css"
+import Header from "./components/Header/Header.jsx";
+import Instructions from "./components/Instructions/Instructions.jsx";
+import Chip from "./components/Chip/Chip.jsx";
+import NutritionalLabel from "./components/NutritionalLabel/NutritionalLabel";
+import { createDataSet } from "./data/dataset";
+import "./App.css";
 
 // don't move this!
 export const appInfo = {
@@ -16,50 +20,92 @@ export const appInfo = {
     noSelectedItem: `Almost there! Choose a menu item and you'll have the fast food facts right at your fingertips!`,
     allSelected: `Great choice! Amazing what a little knowledge can do!`,
   },
-}
+};
 // or this!
-const { data, categories, restaurants } = createDataSet()
+const { data, categories, restaurants } = createDataSet();
 
 export function App() {
+  const [category, setCategory] = React.useState(null);
+  const [restaurant, setRestaurant] = React.useState(null);
+  const [selectedMenuItem, setSelectedMenuItem] = React.useState(null);
+
+  let currentMenuItems = data.filter((d) => {
+    return d.food_category == category && d.restaurant == restaurant;
+  });
+
+  console.log(data);
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
       <div className="CategoriesColumn col">
         <div className="categories options">
           <h2 className="title">Categories</h2>
-          {/* YOUR CODE HERE */}
+          {categories.map((categories) => (
+            <Chip
+              label={categories}
+              key={categories}
+              isActive={categories == category ? true : false}
+              onClick={() => setCategory(categories)}
+            />
+          ))}
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
-        {/* HEADER GOES HERE */}
+        <Header
+          title={appInfo.title}
+          tagline={appInfo.tagline}
+          description={appInfo.description}
+        />
 
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">
+            {restaurants.map((restaurants) => (
+              <Chip
+                label={restaurants}
+                key={restaurants}
+                isActive={restaurants == restaurant ? true : false}
+                onClick={() => setRestaurant(restaurants)}
+              />
+            ))}
+          </div>
         </div>
 
-        {/* INSTRUCTIONS GO HERE */}
-
+        <Instructions instructions={appInfo.instructions.start} />
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
-            {/* YOUR CODE HERE */}
+            {currentMenuItems.map((currentMenuItems, idx) => (
+              <Chip
+                label={currentMenuItems.item_name}
+                key={idx}
+                isActive={
+                  currentMenuItems.item_name == selectedMenuItem ? true : false
+                }
+                onClick={() => {
+                  setSelectedMenuItem(currentMenuItems.food_category);
+                }}
+              />
+            ))}
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts">
+            {/*selectedMenuItem ? (
+              <NutritionalLabel selectedMenuItem={selectedMenuItem} />
+            ) : null*/}
+          </div>
         </div>
-
         <div className="data-sources">
           <p>{appInfo.dataSource}</p>
         </div>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NutritionalLabel/NutritionalLabel.jsx
+++ src/components/NutritionalLabel/NutritionalLabel.jsx
@@ -1,26 +1,35 @@
-import * as React from "react"
-import { nutritionFacts } from "../../constants"
-import "./NutritionalLabel.css"
+import * as React from "react";
+import { nutritionFacts } from "../../constants";
+import "./NutritionalLabel.css";
 
 export function NutritionalLabel(props) {
   return (
     <div className="nutritional-label">
       <h3 className="title">Nutrition Facts</h3>
 
-      <h4 className="item-name">{`CHANGE_ME`}</h4>
+      <h4 className="item-name">{props.selectedMenuItem}</h4>
 
-      <ul className="fact-list">{/* WRITE CODE HERE */}</ul>
+      <ul className="fact-list">
+        {nutritionFacts.forEach((fact) => (
+          <NutritionalLabelFact
+            fact={fact}
+            key={fact.id}
+            label={fact.label}
+            attribute={fact.attribute}
+          />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export function NutritionalLabelFact(props) {
   return (
     <li className="nutrition-fact">
-      <span className="fact-label">{/* WRITE CODE HERE */}</span>{" "}
+      <span className="fact-label">{props.label}</span>{" "}
       <span className="fact-value">{/* WRITE CODE HERE */}</span>
     </li>
-  )
+  );
 }
 
-export default NutritionalLabel
+export default NutritionalLabel;
